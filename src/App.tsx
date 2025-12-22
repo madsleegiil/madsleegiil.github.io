bf94f39fe6ea73aa51428e9756b81cec3ef7e6a8
Mads Lee Giil <mads@giil.no>
2025-12-22 23:25:48 +0100

    Juster på førstesiden

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,17 @@ import {formatDateReadable} from "./utils.ts";
 function App() {
 
     return (
-        <div className="block w-full">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
             {posts.map((post) => (
-                <Link to={`/blog/${post.slug}`} key={post.slug} className="block bg-gray-200 w-full hover:bg-gray-300 transition-colors mb-8">
-                    {post.imagePath && <img src={post.imagePath} alt={"Eksempelbilde for posten"} />}
+                <Link to={`/blog/${post.slug}`} key={post.slug} className="block bg-gray-200 hover:bg-gray-300 transition-colors w-full">
+                    {post.imagePath && (
+                        <img src={post.imagePath} alt="Eksempelbilde for posten" className="w-full"/>
+                    )}
                     <div className="p-6">
                         <h3 className="text-2xl font-bold">{post.title}</h3>
-                        <p className="text-gray-700">{formatDateReadable(post.date)}</p>
+                        <p className="text-gray-700">
+                            {formatDateReadable(post.date)}
+                        </p>
                     </div>
                 </Link>
             ))}
